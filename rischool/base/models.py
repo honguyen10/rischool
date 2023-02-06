534d9c69a2ed51d28992c1402873b0397d6d2a05
honguyen10 <tranhonguyen1999@gmail.com>
2023-02-06 23:39:58 +0700

    [ADD] Add model Student

--- rischool/base/models.py
+++ rischool/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=150)
+    birthday = models.DateField()
+    gender = models.CharField(max_length=10, choices=[
+        ('male', 'Male'),
+        ('female', 'Female')
+    ])
+    emergency_contact_name = models.CharField(max_length=150)
+    emergency_contact_phone = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
