6b889c1599dcddc3da76a83d22d38b973df65b14
honguyen10 <tranhonguyen1999@gmail.com>
2023-02-11 06:41:33 +0000

    ref code and add templates

--- rischool/urls.py
+++ rischool/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_pathr('^ckeditor/', include('ckeditor_uploader.urls')),
+    re_path('^ckeditor/', include('ckeditor_uploader.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
