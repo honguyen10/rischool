6f1b80224e5b6db185472b708f6f8f5b051397b3
honguyen10 <tranhonguyen1999@gmail.com>
2023-02-12 23:32:49 +0700

    [ADD] add relative url'

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,9 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.index, name='index.html'),
+    path('about', views.about, name='about.html'),
+    path('courses', views.courses, name='courses.html'),
+    path('news', views.news, name='news.html'),
+    path('contact', views.contact, name='contact.html'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'base/index.html')
+
+def about(request):
+    return render(request, 'base/about.html')
+
+def courses(request):
+    return render(request, 'base/courses.html')
+
+def news(request):
+    return render(request, 'base/news.html')
+
+def contact(request):
+    return render(request, 'base/contact.html')
